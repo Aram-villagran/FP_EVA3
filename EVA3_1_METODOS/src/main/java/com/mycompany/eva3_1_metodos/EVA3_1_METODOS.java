aba100d7720d2e3a18d10a9819f6968c5ede8e54
Aram-villagran <L24550334@chihuahua2.tecnm.mx>
2024-11-04 16:51:04 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/main/java/com/mycompany/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_metodos;
+
+/**
+ *
+ * @author aramv
+ */
+public class EVA3_1_METODOS {
+
+    public static void main(String[] args) {
+imprimirMensaje ("Hola mundo");
+
+    }
+//declaración del metodo
+    
+    public static void imprimirMensaje(String mensaje){
+        ///cuerpo del método
+        System.out.println("Tu mensaje: " + mensaje);
+    }
+            
+}
+
